acd029abf44017f98ead3bba23cd4fc561ee3c71
Oleksandr Kuzmenko <oleksandr.kuzmenko@rocketroute.com>
2022-05-05 18:42:18 +0300

    First lesson with patterns

---
+++ src/DuckExample/Ducks/ModelDuck.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace OOP\App\DuckExample\Ducks;
+
+use OOP\App\DuckExample\Behavior\FlyNoWay;
+use OOP\App\DuckExample\Behavior\Quack;
+
+class ModelDuck extends Duck
+{
+    public function __construct()
+    {
+        $this->flyBehavior = new FlyNoWay();
+        $this->quackBehavior = new Quack();
+
+    }
+
+
+    public function display()
+    {
+        echo "I'm a model duck!\n";
+    }
+}
