e2e8ce520b336b23e9fb547284f7b46c9e6f5847
Oleksandr Kuzmenko <oleksandr.kuzmenko@rocketroute.com>
2022-06-05 10:59:09 +0300

    Refactoring

--- src/ExamplerSandbox/Application/Controllers/AniexpressController.php
+++ src/ExamplerSandbox/Application/Controllers/AniexpressController.php
@@ -20,65 +20,13 @@ class AniexpressController extends Controller
         $promShop->addItemToCatalog($item2);
         $promShop->addItemToCatalog($item3);
 
-        $htmlItems = '';
-
         $myCatalog = new MyCatalog([$promShop]);
-        while ($myCatalog->hasNext()) {
-            $shop = $myCatalog->next();
-            $iterator = $shop->getIterator();
-            while ($iterator->hasNext()) {
-                $shopItem = $iterator->next();
-                $adapter = $shop->getCatalogItemAdapter($shopItem);
-                $htmlItems .= '<figure class="figure">
-                      <img src="' . $adapter->getItemsShopLogo() . '" class="figure-img img-fluid rounded" alt="...">
-                      <figcaption class="figure-caption">' . $adapter->getItemsName() . ' - $' . $adapter->getItemsPrice() . '$</figcaption>
-                    </figure> ';
-            }
-        }
-
-        echo <<<HTML
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Amster Dam's Pet projects</title>
-        
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-            crossorigin="anonymous">
-           
-           <style>
-           .figure-img {
-                max-width: 250px;
-                max-height: 150px;
-           }
-            </style> 
-        </head>
-        <body>
-            <div class="container-fluid">
-                <div class="jumbotron">
-                    <h1 class="display-4">Anishop agregator</h1>
-                    <p class="lead">Iterator and adapter example</p>
-                    $htmlItems
-                    
-                  </div>
-              </div>
-        </body>
-        </html>
-        HTML;
+        $myCatalog->render($myCatalog->buildCatalogItemsHtml());
     }
 
     public function actionShowprom(): void
     {
-//        <figure class="figure">
-//                      <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" class="figure-img img-fluid rounded" alt="...">
-//                      <figcaption class="figure-caption">A caption for the above image.</figcaption>
-//                    </figure>
-//                    <figure class="figure">
-//                      <img src="https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="figure-img img-fluid rounded" alt="...">
-//                      <figcaption class="figure-caption">A caption for the above image.</figcaption>
-//                    </figure>
+//        https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=
+//        https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
     }
 }
--- src/ExamplerSandbox/Application/Models/AniexpressProject/MyShopAgregator/MyCatalog.php
+++ src/ExamplerSandbox/Application/Models/AniexpressProject/MyShopAgregator/MyCatalog.php
@@ -34,4 +34,58 @@ class MyCatalog implements MyIterator
         $this->counter++;
         return $element;
     }
+
+    public function render(string $htmlItems): void
+    {
+        echo <<<HTML
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Amster Dam's Pet projects</title>
+        
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+            crossorigin="anonymous">
+           
+           <style>
+           .figure-img {
+                max-width: 250px;
+                max-height: 150px;
+           }
+            </style> 
+        </head>
+        <body>
+            <div class="container-fluid">
+                <div class="jumbotron">
+                    <h1 class="display-4">Anishop agregator</h1>
+                    <p class="lead">Iterator and adapter example</p>
+                    $htmlItems
+                    
+                  </div>
+              </div>
+        </body>
+        </html>
+        HTML;
+    }
+
+    public function buildCatalogItemsHtml(): string
+    {
+        $htmlItems = '';
+        while ($this->hasNext()) {
+            $shop = $this->next();
+            $iterator = $shop->getIterator();
+            while ($iterator->hasNext()) {
+                $shopItem = $iterator->next();
+                $adapter = $shop->getCatalogItemAdapter($shopItem);
+                $htmlItems .= '<figure class="figure">
+                      <img src="' . $adapter->getItemsShopLogo() . '" class="figure-img img-fluid rounded" alt="...">
+                      <figcaption class="figure-caption">' . $adapter->getItemsName() . ' - ' . $adapter->getItemsPrice() . '$</figcaption>
+                    </figure> ';
+            }
+        }
+        return $htmlItems;
+    }
 }
